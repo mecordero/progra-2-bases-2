275e772be1d58546f04814408bc514d948e2f4a9
Meli Cordero <melisacorderoarias@hotmail.com>
2018-05-06 23:37:16 -0400

    Add files via upload

--- BODIES/admin_type body.sql
+++ BODIES/admin_type body.sql
@@ -2,7 +2,7 @@ CREATE OR REPLACE
 TYPE BODY ADMIN_TYPE AS
 
   constructor function admin_type(
-        admin_title varchar)
+        admin_title varchar2)
         return self as result AS
   BEGIN
         self.admin_title := admin_title;
