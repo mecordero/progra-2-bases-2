628dc074a29e5d7d9cf77dfade70114378199521
Meli Cordero <melisacorderoarias@hotmail.com>
2018-05-06 23:31:41 -0400

    admin_type body con constructor

---
+++ BODIES/admin_type body.sql
@@ -0,0 +1,14 @@
+CREATE OR REPLACE
+TYPE BODY ADMIN_TYPE AS
+
+  constructor function admin_type(
+        admin_title varchar)
+        return self as result AS
+  BEGIN
+        self.admin_title := admin_title;
+        self.comp_skills := nt_comp_skills();
+        self.office_skills := nt_office_skills();
+    RETURN;
+  END admin_type;
+
+END;
