f62896c093651d3e0ec43a3b70ec209cad5c9572
Daniel Alvarado <dab18@hotmail.com>
2018-05-08 00:37:09 -0600

    Al igual que el insert de enrolls

--- SP CRUDS/Enrolls In/insert_enrrolls_in.sql
+++ SP CRUDS/Enrolls In/insert_enrrolls_in.sql
@@ -1,7 +1,7 @@
 create or replace PROCEDURE insert_enrolls_in
 (
-    student 		REF student_type,
-	degree  		REF degree_type,
+    student_id 		VARCHAR2,
+	  degree_id  		  VARCHAR2,
     result            out real --1 bien 2 mal
 )
 AS 
@@ -10,8 +10,8 @@ BEGIN
   --inserta el objeto en la tabla
   insert into ENROLLS_IN
   VALUES (ENROLLS_IN_TYPE(
-        student,
-        degree));
+        (SELECT REF(s) FROM student s WHERE s.pers_id = student_id),
+        (SELECT REF(d) FROM degree d WHERE d.Deg_ID = degree_id)));
   EXCEPTION
     WHEN DUP_VAL_ON_INDEX THEN
         result := 2;
